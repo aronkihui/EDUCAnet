6c940439bed150cfda6dd77e56a013b2dc93ac35
diegosaavedra <diego_saavedra_1991@hotmail.com>
2014-03-14 04:18:29 -0300

    primeros archivos

--- EDUCAnet/protected/config/main.php
+++ EDUCAnet/protected/config/main.php
@@ -11,7 +11,7 @@ Yii::setPathOfAlias('bootstrap', dirname(__FILE__).'/../extensions/bootstrap');
 //...
 return array(
 	'basePath'=>dirname(__FILE__).DIRECTORY_SEPARATOR.'..',
-	'name'=>'My Web Application',
+	'name'=>'EDUCAnet',
         'theme'=>'bootstrap', // requires you to copy the theme under your themes directory
 
 	// preloading 'log' component
