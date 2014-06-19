2868eccb3cc6f8312e495b84d2d083cb889d5ff7
diegosaavedra <diego_saavedra_1991@hotmail.com>
2014-06-19 05:45:27 -0400

    revision codigo diego

--- EDUCAnet/protected/views/miperfil/_comentarios.php
+++ EDUCAnet/protected/views/miperfil/_comentarios.php
@@ -8,6 +8,23 @@
 
 ?>
 
+<script>
+function comentariosRealizados() {
+    
+
+      document.getElementById('contenedor1').hidden = false;          
+   document.getElementById("contenedor").innerHTML="comentarios realizados";
+}
+
+function comentariosRecibidos()
+{
+    document.getElementById('contenedor2').hidden = false;          
+   
+    document.getElementById("contenedor").innerHTML="comentarios recibidos";
+}
+</script>
+
+
 <h3 class="centered">Todos mis Comentarios</h3>
 
 <div class="container-fluid">  
@@ -19,7 +36,9 @@
     <div class="thumbnail">
     <img src="http://placehold.it/260x180" alt="">
     <h5 class="centered">Comentarios realizados</h5>
-    <p>ver comentarios</p>
+    
+    <div class="centered" > <button  class="btn-large"  onclick="comentariosRealizados()">Comentarios realizados</button> </div>
+    
     </div>
     </li>
     
@@ -27,14 +46,36 @@
     <div class="thumbnail">
     <img src="http://placehold.it/260x180" alt="">
     <h5 class="centered">Comentarios recibidos</h5>
-    <p>ver comentarios</p>
+    
+    <div  class="centered"> <button class="btn-large" onclick="comentariosRecibidos()">Comentarios recibidos</button></div>
+    
     </div>
     </li>
     
     </ul>
    
+   
     
-    
-    
-</div>  
 </div> 
+    
+    
+
+
+<div class="row-fluid">
+  
+
+
+    <div  id="contenedor1" hidden>
+        
+        <p>kledrlkjwelkr</p>
+    </div>
+    
+    <div  id="contenedor2" hidden>
+        
+        <p>kledrlkjwelkr</p>
+    </div>
+</div>
+
+    
+</div> 
+
