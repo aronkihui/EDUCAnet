b070f870c16ed7f625054b3b2f81ebf700167f61
diegosaavedra <diego_saavedra_1991@hotmail.com>
2014-07-07 05:56:55 -0400

    revision codigo diego

--- EDUCAnet/protected/views/matriculador/matriculador.php
+++ EDUCAnet/protected/views/matriculador/matriculador.php
@@ -1,11 +1,21 @@
-<?php
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
-?>
 
-<p> matriculador tiene mantenedor  y formulario de matriculas </p>
+<html>
+</head>
+</head>
+<body>
+    
+    <div class="container-fluid well">
+        
+        <p> matriculador tiene mantenedor  y formulario de matriculas </p> 
+    </div>
+    
+   
+    
+    
+</body>
+    
+    
+</html>
+
